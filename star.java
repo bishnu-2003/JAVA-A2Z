995c78a360fa45637d49043e93f82709f506bb81
bishnu-2003 <bishnugope4844@gmail.com>
2025-12-28 13:37:36 +0530

    Your commit message

---
+++ star.java
@@ -0,0 +1,15 @@
+public class star {
+    public static void main(String[]args){
+
+char c1 ='*';
+for(int i=0; i<5; i++){
+    for(int j=1; j<=i+1; j++){
+        System.out.print(c1);
+    }
+    System.out.println();
+}
+    }
+
+}
+    
+    
